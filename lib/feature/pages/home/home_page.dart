a67c6aae7df218b9feb80cb623d41c0f525e40a9
Vural Kayra Cetintas <73638860+vuralkayracetintas@users.noreply.github.com>
2023-09-11 10:09:03 +0300

    package added

---
+++ lib/feature/pages/home/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Column(children: [
+          Text('text'),
+        ]),
+      ),
+    );
+  }
+}
